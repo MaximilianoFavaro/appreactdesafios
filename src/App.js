372e3b0a58f6daac210e0db5ac89dce09b131526
MaximilianoFavaro <maxi-999@hotmail.com>
2022-07-30 13:19:59 -0300

    Desafio 03 - Contador

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
+import Counter from './components/Counter/Counter';
 
 function App() {
+
+
+  const handleOnAdd = (quantity) => {
+    console.log(`la cantidad agregada es: ${quantity}`)
+  }
+
   return (
     <div className="App">      
         <Navbar/>
         <ItemListContainer greeting='Los mejores muebles'/>
-
+        <Counter stock={10} onAdd={handleOnAdd}/>
     </div>
    
   );
